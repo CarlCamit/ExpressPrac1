b819cca25e36be8d49b5efca19b095be489d22bb
Carl Camit <carlcamit0@gmail.com>
2017-11-23 11:51:14 +1100

    added find to routes

--- src/routes/animals.js
+++ src/routes/animals.js
@@ -6,4 +6,10 @@ router.get("/animals", (req, res) => {
     res.json(Animal.all())
 })
 
+router.get("/animals/:id", (req, res) => {
+    const id = req.params["id"]
+    const animal = Animal.find(id)
+    res.json(animal)
+})
+
 module.exports = router
